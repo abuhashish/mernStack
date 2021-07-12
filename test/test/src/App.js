344314fa32df53f4ce09a88cc8498122968f1494
top1million <aasahashish@gmail.com>
2021-07-12 15:25:52 +0300

    reaact app

---
+++ test/test/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 >Hello Dojo!</h1>
+      <ul>
+        <li>Learn react</li>
+        <li>climb everst</li>
+        <li>run a marathon</li>
+        <li>feed the dogs</li>
+
+      </ul>
+    </div>
+  );
+}
+
+export default App;
